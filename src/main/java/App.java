50efdb415fb384c71748142bd36e40e6c227bc0c
Maksim Bardikov <maximelanosx@yandex.ru>
2023-08-03 00:17:33 +0300

    Add class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,26 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.swing.plaf.IconUIResource;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean3.getMessage());
+        System.out.println(bean4.getMessage());
+        System.out.println(bean3 == bean4);
     }
 }
